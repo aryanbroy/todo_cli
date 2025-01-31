cc1c4ad18ae143db423147fc3175605e7c635187
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-01-31 13:27:41 +0530

    read files from a json file into terminal

--- main.go
+++ main.go
@@ -2,40 +2,67 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"os"
 )
 
+type Todo struct {
+	Todos []TodoContent
+}
+
 type TodoContent struct {
 	Name string `json:"name"`
+	Done bool `json:"done"`
+}
+
+func readExistingData(filename string) ([]TodoContent, error) {
+	data, err := os.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error while reading file")
+		fmt.Println(err)
+		return nil, err
+	}
+
+	var todos []TodoContent
+
+	err = json.Unmarshal(data, &todos)
+
+	return todos,err 
 }
 
 func main() {
-	task := flag.String("add", "[Empty]", "Add a task")
+	// task := flag.String("add", "[Empty]", "Add a task")
+	// done := flag.Bool("done", false, "Mark a task")
 
-	flag.Parse()
+	// flag.Parse()
 
-	nameJson, err := json.MarshalIndent(TodoContent{Name: *task}, "", " ")
+	// nameJson, err := json.MarshalIndent(TodoContent{Name: *task, Done: *done}, "", " ")
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// file, err := os.Create("todos.json")
+
+	// defer file.Close()
+
+	var todo []TodoContent 
+
+	todo, err := readExistingData("todos.json")
 
 	if err != nil {
 		panic(err)
 	}
 
-	file, err := os.Create("todos.json")
+	fmt.Println(todo[0])
 
-	if err != nil {
-		panic(err)
-	}
+	// _, err = file.Write(nameJson)
 
-	defer file.Close()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	_, err = file.Write(nameJson)
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Task added: ", *task)
-	fmt.Println(string(nameJson))
+	// fmt.Println("Task added: ", *task)
+	// fmt.Println(string(nameJson))
 }
