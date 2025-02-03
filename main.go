60b026c2daf9af9e9f91d8b54fed81da0fcd28ed
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-02-03 18:50:58 +0530

    implement delete task functionality

--- main.go
+++ main.go
@@ -41,36 +41,61 @@ func main() {
 	task := flag.String("add", "[Empty]", "Add a task")
 	done := flag.Bool("done", false, "Mark a task")
 	display := flag.Bool("display", false, "Display todo list")
+	deleteTaskId := flag.Int("del", -1, "Delete a specific task using its id")
 
 	filename := "todos.json"
 	flag.Parse()
 
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"ID", "NAME", "ISDONE"})
-
-	if *display {
-		var todoList []TodoContent
-		todoList, err := readExistingData(filename)
-		if err != nil {
-			fmt.Println("Error displaying the list of todos!")
-			panic(err)
-		}
-		for _, item := range todoList {
-			t.AppendRow(table.Row{item.Id, item.Name, item.Done})
-			// fmt.Println("Id: ", item.Id, "Name: ", item.Name, "IsDone: ", item.Done)
-		}
-		t.Render()
-		return
-	}
-
-	taskByte, err := json.MarshalIndent(TodoContent{Name: *task, Done: *done}, "", " ")
+	var todos []TodoContent
+	todos, err := readExistingData(filename)
 
 	if err != nil {
 		panic(err)
 	}
 
-	todos, err := readExistingData(filename)
+	if *deleteTaskId > 0 {
+		indexToDel := -1
+		for i, item := range todos {
+			if item.Id == *deleteTaskId {
+				indexToDel = i
+				break
+			}
+		}
+
+		if indexToDel == -1 {
+			fmt.Println("No such task to delete...")
+			return
+		}
+		todos = append(todos[ :indexToDel], todos[indexToDel + 1 : ]...)
+		byteSlice := []byte{}	
+		for _, item := range todos {
+			itemBytes, err := json.Marshal(item)	
+			if err != nil {
+				panic(err)
+			}
+			byteSlice = append(byteSlice, itemBytes...)
+		}
+		err := saveTodo(filename, byteSlice)
+		if err != nil {
+			panic(err)
+		}
+		// fmt.Println(todos)
+		return
+	}
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"ID", "NAME", "ISDONE"})
+
+	if *display {	
+		for _, item := range todos {
+			t.AppendRow(table.Row{item.Id, item.Name, item.Done})
+		}
+		t.Render()
+		return
+	}
+
+	taskByte, err := json.MarshalIndent(TodoContent{Name: *task, Done: *done}, "", " ")	
 
 	if err != nil {
 		taskByte, error := json.MarshalIndent([]TodoContent{{Id: 1, Name: *task, Done: *done}}, "", " ")
